08c01d940907e2d664a6e772de613baefa997f80
Oğuz Batuhan <oguzbatuhanczl@gmail.com>
2024-10-10 11:13:28 +0300

    veriables types

---
+++ Javascript: section-1/5_veriables_types/javascript.js
@@ -0,0 +1,41 @@
+// veriables types
+
+var productName ="Iphone 16"; // string
+var productPrice ="70000"; // string 
+var productPrice2 =70000; // number
+
+console.log(typeof productName);
+console.log(typeof productPrice);
+console.log(typeof productPrice2);
+
+var number1 ="10";
+var number2 ="20";
+
+console.log(number1 + number2);
+
+number1 =10;
+number2 =20;
+
+console.log(number1 + number2);
+
+var userName ="Oğuz Batuhan";
+var userSurname ="Çözeli";
+
+console.log(userName + " " + userSurname);
+
+var userName1 ="Stev";
+var examScore1 =40;
+var IsİtSuccessful1 =(examScore1 >=50); // boolean = true, false
+
+console.log(userName1 + " Is İt Successful " + IsİtSuccessful1);
+
+var userName2 ="Aym";
+var examScore2 =70;
+var IsİtSuccessful2 =(examScore2 >=50); // boolean = true, false
+
+console.log(userName2 + " Is İt Successful " + IsİtSuccessful2);
+
+
+var old;
+console.log(old);
+console.log(typeof old);
