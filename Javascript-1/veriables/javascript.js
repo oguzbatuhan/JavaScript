971c4f228abe37baa700b131f71503eb6b4ef41f
Oğuz Batuhan <oguzbatuhanczl@gmail.com>
2024-10-10 10:40:51 +0300

    veriables

---
+++ Javascript-1/veriables/javascript.js
@@ -0,0 +1,18 @@
+// Veriables - Değişkenler
+
+let a = 5500;
+let b = 5000;
+var c = 6000;
+var d = 7000;
+var kdv = 1.2;
+
+console.log(a * kdv); // ürün a
+
+a = 9000;
+
+console.log(a * kdv); // ürün a
+console.log(b * kdv); // ürün b
+console.log(c * kdv); // ürün c
+console.log(d * kdv); // ürün d
+
+// değişken tanımlanırken komut ve türkçe karakter kullanılmaz aynı zamanda rakamla başlayamaz arada boşluk ve - kullanılamaz.
