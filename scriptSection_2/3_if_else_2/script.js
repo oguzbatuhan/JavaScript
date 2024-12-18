02d73cbb230f105b20e843224dde01e9eaba9089
Oğuz Batuhan <oguzbatuhanczl@gmail.com>
2024-12-18 14:59:57 +0300

    if/else conditionals

---
+++ scriptSection_2/3_if_else_2/script.js
@@ -0,0 +1,14 @@
+// if/else koşul ifadeleri.
+
+// old >= 18
+// graduation == high school or graduation == university
+
+let old = 18;
+let graduation = "university";
+
+if ((old >= 18) && ((graduation == "high school") || (graduation == "university"))) {
+    console.log("can get a driver's license");
+}
+else {
+    console.log("can't get a driver's license");
+}
