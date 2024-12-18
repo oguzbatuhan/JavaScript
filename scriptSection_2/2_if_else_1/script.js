980160704076a7d7baa0f01803f1c649c29509e9
Oğuz Batuhan <oguzbatuhanczl@gmail.com>
2024-12-18 14:15:22 +0300

    if/else conditionals

---
+++ scriptSection_2/2_if_else_1/script.js
@@ -0,0 +1,16 @@
+// İf Else koşul ifadeleri
+
+let username = "oguzbatuhan";
+let password = "12345";
+
+if (username == "oguzbatuhan") {
+    if (password == "12345") {
+        console.log("user login successful.")
+    }
+    else {
+        console.log("userpassword is incorrect")
+    }
+}
+else {
+    console.log("username is incorrect")
+}
